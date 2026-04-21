a211c2022af6a0ad3224a63ad420f6cbb1985c63
ecxqua <pagiloy@gmail.com>
2026-04-21 19:23:44 +0500

    GA started

--- config.py
+++ config.py
@@ -15,8 +15,14 @@ ENERGY_THRESHOLD: Final[float] = 40.0  # мин. энергия для разм�
 SEEK_SPEED: Final[float] = 80.0          # Скорость движения к еде
 PERCEPTION_RADIUS: Final[float] = 100.0  # Радиус "зрения" существа
 
-# ГА (заглушки на потом)
-MUTATION_RATE: Final[float] = 0.1
-TOURNAMENT_SIZE: Final[int] = 3
+# ГА
+POPULATION_SIZE: Final[int] = 20      # Размер популяции
+ELITISM_COUNT: Final[int] = 2         # Сколько лучших сохраняются без изменений
+TOURNAMENT_SIZE: Final[int] = 3       # Размер турнира для отбора
+MUTATION_RATE: Final[float] = 0.1     # Базовая вероятность мутации гена
+MUTATION_STRENGTH: Final[float] = 0.2 # Насколько сильно может измениться ген
+MIN_MUTATION_RATE: Final[float] = 0.02  # Для адаптивности
+MAX_MUTATION_RATE: Final[float] = 0.3
+DIVERSITY_THRESHOLD: Final[float] = 0.15  # Порог для адаптивной мутации
 
 MAX_STEERING_FORCE: Final[float] = 20.0  # Максимальная сила поворота/ускоренияz
